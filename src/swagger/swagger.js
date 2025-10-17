1af7eb8bf3c0d1fddde844d881fa536c960c5f6b
Naman Muley <naman.g.muley@gmail.com>
2025-10-16 18:49:08 -0700

    Add src/swagger/swagger.js

---
+++ src/swagger/swagger.js
@@ -0,0 +1,30 @@
+// src/swagger/swagger.js
+const swaggerUi = require('swagger-ui-express');
+const swaggerJSDoc = require('swagger-jsdoc');
+
+const options = {
+  definition: {
+    openapi: '3.0.1',
+    info: {
+      title: 'Task Manager API',
+      version: '1.0.0',
+      description: 'Secure Task Management REST API',
+    },
+    servers: [{ url: '/api' }],
+    components: {
+      securitySchemes: {
+        bearerAuth: {
+          type: 'http',
+          scheme: 'bearer',
+        },
+      },
+    },
+    security: [{ bearerAuth: [] }],
+  },
+  apis: ['./src/routes/*.js'],
+};
+
+const swaggerSpec = swaggerJSDoc(options);
+module.exports = {
+  swaggerUi, swaggerSpec,
+};
